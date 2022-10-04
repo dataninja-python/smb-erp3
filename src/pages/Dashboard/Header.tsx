a99b93258a37c1b17fc242b4a00bc46d68586307
AJ Igherighe <the.coders.dao@gmail.com>
2022-10-04 19:25:26 -0400

    updated header

--- src/pages/Dashboard/Header.tsx
+++ src/pages/Dashboard/Header.tsx
@@ -3,7 +3,7 @@ import React, { SetStateAction } from 'react';
 function Header({ setIsAdding }: { setIsAdding: React.Dispatch<SetStateAction<boolean>> }) {
     return (
         <header>
-            <h1>SMB ERP: Staff Management Module</h1>
+            <h2>SMB ERP: Admin Staff Management Module</h2>
             <div style={{ marginTop: '30px', marginBottom: '18px' }}>
                 <button onClick={() => setIsAdding(true)} className='round-button'>Add Employee</button>
             </div>
