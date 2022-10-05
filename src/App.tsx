a7320f23d2f4c8a3f6d219f37b71bb1491f2b16a
AJ Igherighe <the.coders.dao@gmail.com>
2022-10-04 23:48:16 -0400

    attempted fix 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 //import React from 'react';
 //import logo from './logo.svg';
 //import './App.css';
-import StaffManager from './pages/StaffManager';
+import StaffManager from './pages/StaffManager/StaffManager';
 
 function App() {
     return (
